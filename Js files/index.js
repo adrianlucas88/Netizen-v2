52a73c9c39a00f590c662c419a3158ca5dfa44c4
adrianlucas88 <pasha2366@gmail.com>
2025-06-15 21:40:52 +0100

    Remove typewriter effect and update introduction text in index.html; simplify CSS by removing transition on anchor tags

--- Js files/index.js
+++ Js files/index.js
@@ -5,7 +5,6 @@ let hello3 = document.getElementById("hello3");
 let tapMob = document.getElementById("mob");
 hello1.addEventListener("mouseover", stat1);
 hello1.addEventListener("mouseout", back);
-hello1.addEventListener("click", typewriter);
 hello2.addEventListener("mouseover", stat2);
 hello2.addEventListener("mouseout", back);
 hello3.addEventListener("mouseover", stat3);
@@ -42,41 +41,6 @@ function tapScreen() {
   } else if (hello1.innerHTML == "About" && window.innerWidth <= 1100) {
     back();
     console.log("double tap");
-  }
-}
-
-// Text animation
-// set up text to print, each item in array is new line
-let aText = new Array(
-  " Hi, I'm Adrian, a Frontend Developer with a passion for creating intuitive and user-friendly websites.<br /><br />As a Frontend Developer, I bring a fresh perspective to every project, along with a hunger to learn and grow. I have a solid foundation in HTML, CSS, and JavaScript.<br /><br /> What sets me apart is my dedication to creating user-centered designs. I believe that great design is about more than just making things look pretty - it's about creating an experience that's engaging, intuitive, and easy to use. That's why I always start with a deep understanding of the end user, conducting user research and testing to ensure that every design decision is backed by data and user feedback.<br /><br /> Whether you need a new website, a redesign of an existing one, or a custom web application, I'm here to help. I'm a fast learner, a team player, and I'm committed to delivering high-quality work that exceeds your expectations.<br /><br /> So if you're looking for a Frontend Developer with a passion for user experience and a hunger to learn, look no further. Let's work together to create something amazing!"
-);
-let iSpeed = 50; // time delay of print out
-let iIndex = 0; // start printing array at this posision
-let iArrLength = aText[0].length; // the length of the text array
-let iScrollAt = 20; // start scrolling up at this many lines
-
-let iTextPos = 0; // initialise text position
-let sContents = ""; // initialise contents variable
-let iRow; // initialise current row
-
-function typewriter() {
-  sContents = " ";
-  iRow = Math.max(0, iIndex - iScrollAt);
-  let destination = document.getElementById("typeD");
-
-  while (iRow < iIndex) {
-    sContents += aText[iRow++] + "<br />";
-  }
-  destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos);
-  if (iTextPos++ == iArrLength) {
-    iTextPos = 0;
-    iIndex++;
-    if (iIndex != aText.length) {
-      iArrLength = aText[iIndex].length;
-      setTimeout("typewriter()", 500);
-    }
-  } else {
-    setTimeout("typewriter()", iSpeed);
   }
 }
 
